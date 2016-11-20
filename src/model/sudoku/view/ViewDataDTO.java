9cb64f7c75fb58814a372826ed50ff4be4aa20b7
Vilkaz <admin@vilkas.de>
2016-11-20 10:10:56 +0100

    barcelonas work, pushing

---
+++ src/model/sudoku/view/ViewDataDTO.java
@@ -0,0 +1,39 @@
+package model.sudoku.view;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import model.sudoku.Sudoku;
+
+/**
+ * Created by vkukanauskas on 05/11/2016.
+ */
+public class ViewDataDTO {
+    private Pane mainPane;
+    private GridPane sudokuGrid;
+    private Sudoku sudoku;
+    private ActionEvent event;
+
+    public ViewDataDTO(Pane mainPane, GridPane sudokuGrid, Sudoku sudoku, ActionEvent event) {
+        this.mainPane = mainPane;
+        this.sudokuGrid = sudokuGrid;
+        this.sudoku = sudoku;
+        this.event = event;
+    }
+
+    public Pane getMainPane() {
+        return mainPane;
+    }
+
+    public GridPane getSudokuGrid() {
+        return sudokuGrid;
+    }
+
+    public Sudoku getSudoku() {
+        return sudoku;
+    }
+
+    public ActionEvent getEvent() {
+        return event;
+    }
+}
